56f92c997ecb8402eb10b4d2b5eedef6e2d1510b
AJ <abduljaberaj00786@gmail.com>
2025-04-12 09:39:38 +0530

    chore: fixes

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6 }}
-      className="relative w-full min-h-dvh flex flex-col py-48 items-center text-center overflow-hidden"
+      className="relative w-full min-h-dvh flex flex-col py-44 items-center text-center overflow-hidden"
     >
       {/* Hero Content */}
       <div className="z-10 max-w-2xl">
@@ -17,9 +17,9 @@ const Hero = () => {
           initial={{ opacity: 0, filter: "blur(10px)" }}
           animate={{ opacity: 1, filter: "blur(0px)" }}
           transition={{ delay: 0.2, duration: 0.6 }}
-          src="avatar.png"
+          src="/assets/ME.jpg"
           alt="My Avatar"
-          className="mx-auto mb-6 w-40 h-40 rounded-full border-4 border-primary shadow-lg object-cover"
+          className="mx-auto mb-6 h-40 relative rounded-[100%] border-4 border-primary shadow-lg object-cover"
         />
 
         <motion.h1
@@ -29,7 +29,9 @@ const Hero = () => {
           className="text-4xl md:text-5xl font-bold mb-4 text-foreground"
         >
           Hey, I'm{" "}
-          <span className="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent"></span>
+          <span className="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent">
+            Abdul Jaber
+          </span>
         </motion.h1>
         <motion.h2
           initial={{ opacity: 0, y: 10 }}
@@ -40,11 +42,11 @@ const Hero = () => {
           <TypeAnimation
             sequence={[
               "Aspiring Full Stack Developer",
-              1500,
+              2000,
               "Student",
-              1500,
+              2000,
               "Loves Learning New Things",
-              1500,
+              2000,
             ]}
             wrapper="span"
             speed={50}
